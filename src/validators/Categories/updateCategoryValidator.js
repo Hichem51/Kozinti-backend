6d2659bd84a7b2f9894a750a52ebc23245d7f2d7
medali213 <mmed.ali2255@gmail.com>
2026-04-27 16:58:58 +0100

    added crud for category

--- src/validators/Categories/updateCategoryValidator.js
+++ src/validators/Categories/updateCategoryValidator.js
@@ -2,9 +2,10 @@ const { body } = require("express-validator");
 
 module.exports = [
   body("name")
+    .optional()
     .trim()
     .notEmpty()
-    .withMessage("Category name is required")
+    .withMessage("Category name cannot be empty")
     .isLength({ max: 80 })
     .withMessage("Category name cannot exceed 80 characters"),
 
